0259f93fc1d1849e0a26889f7ca55d93054ee12d
Solium Dev <lucas.develop.prog@gmail.com>
2023-12-01 12:05:29 -0300

    feat: pinia and vue-router used by vue

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { router } from "./shared/lib/router";
+import pinia from "./shared/lib/pinia";
+const app = createApp(App);
+app.use(router);
+app.use(pinia);
 
-createApp(App).mount('#app')
+app.mount("#app");
