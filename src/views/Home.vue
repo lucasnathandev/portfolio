84bca562cda4dcc501d4b5d6c4583453db582dc9
Solium Dev <lucas.develop.prog@gmail.com>
2023-12-02 09:28:29 -0300

    feat: added tailwind colors and h1 in home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,6 +7,7 @@ import HomeNavBar from '@/resources/home/components/HomeNavBar.vue';
 
 <template>
   <header class="px-2 w-full h-screen">
+    <h1 class="text-3xl w-full font-bold py-2 px-1 text-center text-anti-flash_white">Lucas Nathan Henrique da Silva</h1>
     <HomeNavBar />
   </header>
 </template>
