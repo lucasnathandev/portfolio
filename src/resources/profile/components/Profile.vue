61e283a54708b1451773a7477c8b156d15bf535d
Solium Dev <lucas.develop.prog@gmail.com>
2023-12-03 14:37:17 -0300

    feat: Profile component added

---
+++ src/resources/profile/components/Profile.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section class="profile-section">
+    <div class="profile-photo-container"></div>
+  </section>
+</template>
+
+<style scoped>
+.profile-section {
+  @apply flex flex-1 items-center justify-between flex-col
+}
+
+.profile-photo-container {
+  @apply w-full h-full min-h-[fit-content]
+}
+</style>
