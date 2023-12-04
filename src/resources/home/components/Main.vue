93c2e82e8aaa8f68631986ad71ec12638e1de843
Solium Dev <lucas.develop.prog@gmail.com>
2023-12-03 21:16:12 -0300

    feat: Main component added to home

---
+++ src/resources/home/components/Main.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main class="home-main">
+
+  </main>
+</template>
+
+<style scoped>
+.home-main {
+  @apply flex flex-1 min-h-[320px] items-center justify-around w-full h-full;
+  grid-area: "main";
+}
+</style>
