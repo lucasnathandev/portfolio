e72d12f4c4b0bf3ecdfedf4e74c5e9a72f4d2531
Solium Dev <lucas.develop.prog@gmail.com>
2023-12-03 21:15:37 -0300

    refactor: header tag moved to SFC

---
+++ src/resources/home/components/Header.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Social from '@/resources/home/components/Social.vue';
+import Profile from '@/resources/profile/components/Profile.vue';
+</script>
+
+
+<template>
+  <header class="px-2 w-full home-header">
+    <h1 class="text-3xl w-full font-bold py-3 text-center text-anti-flash_white">Lucas Nathan Henrique da Silva</h1>
+    <Profile />
+    <Social />
+  </header>
+</template>
+
+<style scoped>
+.home-header {
+  grid-area: "head"
+}
+</style>
